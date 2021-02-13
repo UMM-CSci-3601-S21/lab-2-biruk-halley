961ae212828545f3922a777acab0f412a3a8f1b1
Biruk <mengi024@morris.umn.edu>
2021-02-13 14:29:45 -0600

    Add server endpoint for todos #1
    Server now replies to requests made to /todos. Also initialized controllers for the todos inside server.java. Co-authored-by:@halleypaulson

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -4,19 +4,25 @@ import java.io.IOException;
 
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
+import umm3601.todo.TodoController;
 import umm3601.user.Database;
 import umm3601.user.UserController;
 
 public class Server {
 
   public static final String CLIENT_DIRECTORY = "../client";
+
   public static final String USER_DATA_FILE = "/users.json";
+  public static final String TODO_DATA_FILE = "/todos.json";
+
   private static Database userDatabase;
+  private static umm3601.todo.Database todoDatabase;
 
   public static void main(String[] args) {
 
     // Initialize dependencies
     UserController userController = buildUserController();
+    TodoController todoController = buildTodoCotroller();
 
     Javalin server = Javalin.create(config -> {
       // This tells the server where to look for static files,
@@ -39,6 +45,32 @@ public class Server {
 
     // List users, filtered using query parameters
     server.get("/api/users", ctx -> userController.getUsers(ctx));
+
+    // List todos, filter using query parameters
+    server.get("/api/todos", ctx -> todoController.getTodos(ctx));
+
+  }
+
+    /***
+   * Create a database using the json file, use it as data source for a new
+   * TodoController
+   *
+   * Constructing the controller might throw an IOException if there are problems
+   * reading from the JSON "database" file. If that happens we'll print out an
+   * error message exit the program.
+   */
+  private static TodoController buildTodoCotroller() {
+    TodoController todoController = null;
+    try {
+      todoDatabase = new umm3601.todo.Database(TODO_DATA_FILE);
+      todoController = new TodoController(todoDatabase);
+    } catch (IOException e) {
+      System.err.println("The server failed to load the todo data; shutting down.");
+      e.printStackTrace(System.err);
+      System.exit(1);
+    }
+
+    return todoController;
   }
 
   /***
