bf9e7b747cba0fb0a92387978f7865efdfcd71ea
Biruk <mengi024@morris.umn.edu>
2021-02-13 14:08:53 -0600

    Add tests for fetching all todos and implement methods #1
    Designed tests for the getTodos method and built the method using the tests. Co-authored-by:@halleypaulson

--- server/src/main/java/umm3601/todo/Database.java
+++ server/src/main/java/umm3601/todo/Database.java
@@ -3,6 +3,8 @@ package umm3601.todo;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import com.google.gson.Gson;
 
@@ -26,5 +28,20 @@ public class Database {
 
   public Todo getTodo(String id) {
     return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+  }
+
+  public Todo[] listTodos(Map<String, List<String>> queryParams) {
+    Todo[] filteredTodos = allTodos;
+
+    // Add filters here...
+
+    // owner filter
+
+    // category filter
+
+    // status filter
+
+
+    return filteredTodos;
   }
 }
