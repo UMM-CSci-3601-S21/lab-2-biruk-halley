22cad00a09777fbff51915d9009ab325dfb779ea
Halley Paulson <paul1098@morris.umn.edu>
2021-02-15 12:45:32 -0600

    Implement methods and add comments #4
    Create and implement method to make tests from previous commit pass. Add comments for 3 filter methods in database so users will know what they do.

--- server/src/main/java/umm3601/todo/Database.java
+++ server/src/main/java/umm3601/todo/Database.java
@@ -7,8 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
-
-import io.javalin.Javalin;
 import io.javalin.http.BadRequestResponse;
 
 /**
@@ -65,6 +63,7 @@ public class Database {
 
     if (queryParams.containsKey("limit")) {
       String limitParam = queryParams.get("limit").get(0);
+
       try {
         int targetAge = Integer.parseInt(limitParam);
         filteredTodos = limitTodos(filteredTodos, targetAge);
@@ -84,15 +83,45 @@ public class Database {
       filteredTodos = filterTodosByStatus(filteredTodos, statusParam);
     }
 
+    // contains filter
+
+    if (queryParams.containsKey("contains")) {
+      String containsParam = queryParams.get("contains").get(0);
+
+      filteredTodos = searchTodosByKeyWord(filteredTodos, containsParam);
+    }
+
     return filteredTodos;
   }
 
+  /**
+   * Used in listTodos(). Limits amount of todo JSON objects displayed.
+   * @param todos
+   * @param targetLimit
+   * @return array of limited todos
+   */
   public Todo[] limitTodos(Todo[] todos, int targetLimit) {
     return Arrays.stream(todos).limit(targetLimit).toArray(Todo[]::new);
   }
 
+  /**
+   * Used in listTodos(). Searches for todo JSON objects with designated status.
+   * @param todos
+   * @param status
+   * @return array of todos with designated status (Complete, Incomplete).
+   */
   public Todo[] filterTodosByStatus(Todo[] todos, String status) {
     return Arrays.stream(todos).filter(each -> each.status == "complete".equals(status))
     .toArray(Todo[]::new);
+  }
+
+  /**
+   * Used in listTodos(). Searches body of todo JSON object to see if it contains given key word.
+   * @param todos
+   * @param keyWord
+   * @return array of todos with bodies containing given key word.
+   */
+  public Todo[] searchTodosByKeyWord(Todo[] todos, String keyWord){
+    return Arrays.stream(todos).filter(x -> x.body.contains(keyWord)).toArray(Todo[]::new);
   }
 }
